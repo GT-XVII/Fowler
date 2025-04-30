d236b50a1bcf9e77e9e40c02e277efd5c49430e7
GT-XVII <liamthomson118@gmail.com>
2025-04-30 17:20:44 +0200

    Customer Refactor

--- src/Customer.java
+++ src/Customer.java
@@ -1,69 +1,88 @@
+import java.util.Enumeration;
+import java.util.Vector;
 
-import java.lang.*;
-import java.util.*;
-
+/**
+ * Represents a customer who rents movies.
+ * Tracks rental records and calculates statements and frequent renter points.
+ */
 class Customer {
     private String name;
-    private Vector rentals = new Vector();
-    public Customer (String newname){
+    private Vector<Rental> rentals = new Vector<>();
+
+    /**
+     * Constructs a new Customer with a given name.
+     *
+     * @param newname the name of the customer
+     */
+    public Customer(String newname) {
         name = newname;
-    };
+    }
+
+    /**
+     * Adds a rental to the customer's record.
+     *
+     * @param arg the rental to add
+     */
     public void addRental(Rental arg) {
         rentals.addElement(arg);
-    };
-    public String getName (){
+    }
+
+    /**
+     * Returns the name of the customer.
+     *
+     * @return the customer's name
+     */
+    public String getName() {
         return name;
-    };
+    }
+
+    /**
+     * Generates a detailed rental statement for the customer.
+     *
+     * @return the formatted rental statement
+     */
     public String statement() {
-        double totalAmount = 0;
-        Enumeration enum_rentals = rentals.elements();
+        Enumeration<Rental> enum_rentals = rentals.elements();
         String result = "Rental Record for " + this.getName() + "\n";
-        result += "\t" + "Title" + "\t" + "\t" + "Days" + "\t" + "Amount" + "\n";
+        result += "\tTitle\t\tDays\tAmount\n";
 
         while (enum_rentals.hasMoreElements()) {
-            Rental each = (Rental) enum_rentals.nextElement();
-            //determine amounts for each line
-            //show figures for this rental
-            result += "\t" + each.getMovie().getTitle()+ "\t" + "\t" + each.getDaysRented() + "\t" + String.valueOf(each.getCharge()) + "\n";
+            Rental each = enum_rentals.nextElement();
+            result += "\t" + each.getMovie().getTitle() + "\t\t" + each.getDaysRented() + "\t" + each.getCharge() + "\n";
         }
-        //add footer lines
-        result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
-        result += "You earned " + String.valueOf(getTotalFrequentRenterPoints()) + " frequent renter points";
+
+        result += "Amount owed is " + getTotalCharge() + "\n";
+        result += "You earned " + getTotalFrequentRenterPoints() + " frequent renter points";
         return result;
     }
 
-//    It is worth stopping to think a bit about the last refactoring. Most refactorings reduce the amount
-//    of code, but this one increases it. That's because Java 1.1 requires a lot of statements to set up a
-//    summing loop. Even a simple summing loop with one line of code per element needs six lines of
-//    support around it. It's an idiom that is obvious to any programmer but is a lot of lines all the same.
-//    The other concern with this refactoring lies in performance. The old code executed the "while"
-//    loop once, the new code executes it three times. A while loop that takes a long time might impair
-//    performance. Many programmers would not do this refactoring simply for this reason. But note
-//    the words if and might. Until I profile I cannot tell how much time is needed for the loop to
-//    calculate or whether the loop is called often enough for it to affect the overall performance of the
-//    system. Don't worry about this while refactoring. When you optimize you will have to worry about
-//    it, but you will then be in a much better position to do something about it, and you will have more
-//    options to optimize effectively (see the discussion on page 69).
-
+    /**
+     * Calculates the total charge for all rentals.
+     *
+     * @return the total charge amount
+     */
     private double getTotalCharge() {
         double result = 0;
-        Enumeration enum_rentals = rentals.elements();
+        Enumeration<Rental> enum_rentals = rentals.elements();
         while (enum_rentals.hasMoreElements()) {
-            Rental each = (Rental) enum_rentals.nextElement();
+            Rental each = enum_rentals.nextElement();
             result += each.getCharge();
         }
         return result;
     }
 
-    private int getTotalFrequentRenterPoints(){
+    /**
+     * Calculates the total frequent renter points earned.
+     *
+     * @return the total frequent renter points
+     */
+    private int getTotalFrequentRenterPoints() {
         int result = 0;
-        Enumeration enum_rentals = rentals.elements();
+        Enumeration<Rental> enum_rentals = rentals.elements();
         while (enum_rentals.hasMoreElements()) {
-            Rental each = (Rental) enum_rentals.nextElement();
+            Rental each = enum_rentals.nextElement();
             result += each.getFrequentRenterPoints();
         }
         return result;
     }
-
 }
-    
