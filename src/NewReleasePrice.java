0cbd43f45e929bdb24375785ded77ffbbcc8036a
GT-XVII <liamthomson118@gmail.com>
2025-04-30 17:22:48 +0200

    New release price refactor

--- src/NewReleasePrice.java
+++ src/NewReleasePrice.java
@@ -1,22 +1,36 @@
 /**
- * Created with IntelliJ IDEA.
- * User: Berkling
- * Date: 14.05.14
- * Time: 12:49
- * To change this template use File | Settings | File Templates.
+ * Represents the pricing strategy for new release movies.
+ * Applies a flat rate per day and provides extra renter points.
  */
 public class NewReleasePrice implements Price {
-    public int getPriceCode(){
+
+    /**
+     * Returns the price code for new release movies.
+     *
+     * @return the price code for new releases
+     */
+    public int getPriceCode() {
         return Movie.NEW_RELEASE;
     }
 
-    public double getCharge(int daysRented){
-        return (daysRented * 3 );
+    /**
+     * Calculates the charge for renting a new release movie.
+     *
+     * @param daysRented the number of days rented
+     * @return the charge based on days rented
+     */
+    public double getCharge(int daysRented) {
+        return daysRented * 3;
     }
 
+    /**
+     * Returns the frequent renter points for a new release movie.
+     * Always returns 2 points regardless of rental duration.
+     *
+     * @param daysRented the number of days the movie is rented
+     * @return frequent renter points earned
+     */
     public int getFrequentRenterPoints(int daysRented) {
         return 2;
     }
-
 }
-
