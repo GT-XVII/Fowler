bdcc7b73cdc98d342148efb119f57701cfad90a5
GT-XVII <liamthomson118@gmail.com>
2025-04-30 17:24:00 +0200

    Price refactor

--- src/Price.java
+++ src/Price.java
@@ -1,12 +1,29 @@
 /**
- * Created with IntelliJ IDEA.
- * User: Berkling
- * Date: 14.05.14
- * Time: 12:48
- * To change this template use File | Settings | File Templates.
+ * Represents a pricing strategy for a movie category.
+ * Implementing classes define specific behavior for charges and frequent renter points.
  */
 public interface Price {
-    abstract int getPriceCode();
-    abstract double getCharge(int daysRented);
-    abstract int getFrequentRenterPoints(int daysRented);
+
+    /**
+     * Gets the price code representing the movie category.
+     *
+     * @return the price code
+     */
+    int getPriceCode();
+
+    /**
+     * Calculates the rental charge based on number of days rented.
+     *
+     * @param daysRented the number of days the item is rented
+     * @return the calculated charge
+     */
+    double getCharge(int daysRented);
+
+    /**
+     * Calculates the frequent renter points earned based on rental duration.
+     *
+     * @param daysRented the number of days the item is rented
+     * @return the earned frequent renter points
+     */
+    int getFrequentRenterPoints(int daysRented);
 }
