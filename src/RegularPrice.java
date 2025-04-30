ad833fd38627cfc50e46a578e19631eed2d88b46
GT-XVII <liamthomson118@gmail.com>
2025-04-30 17:26:32 +0200

    Regular Price

--- src/RegularPrice.java
+++ src/RegularPrice.java
@@ -1,23 +1,41 @@
 /**
- * Created with IntelliJ IDEA.
- * User: Berkling
- * Date: 14.05.14
- * Time: 12:49
- * To change this template use File | Settings | File Templates.
+ * Represents the pricing strategy for regular movies.
+ * Applies a base charge with additional cost after a threshold.
  */
 public class RegularPrice implements Price {
-    public int getPriceCode(){
+
+    /**
+     * Returns the price code for regular movies.
+     *
+     * @return the price code for regular category
+     */
+    public int getPriceCode() {
         return Movie.REGULAR;
     }
 
-    public double getCharge(int daysRented){
-        if (daysRented > 2) return (daysRented - 2 ) * 1.5;
-        else return 2;
+    /**
+     * Calculates the charge for renting a regular movie.
+     *
+     * @param daysRented the number of days the movie is rented
+     * @return the charge based on rental duration
+     */
+    public double getCharge(int daysRented) {
+        if (daysRented > 2) {
+            return (daysRented - 2) * 1.5 + 2;
+        } else {
+            return 2;
+        }
     }
+
+    /**
+     * Returns the frequent renter points for a regular movie.
+     *
+     * @param daysRented the number of days the movie is rented
+     * @return the number of frequent renter points
+     */
     public int getFrequentRenterPoints(int daysRented) {
         return 1;
     }
-
 }
 
 
