077d1bd7cb3c4403cd941cdcea9c8fc93cd599d7
GT-XVII <liamthomson118@gmail.com>
2025-04-30 17:19:49 +0200

    Childrens price refactor

--- src/ChildrensPrice.java
+++ src/ChildrensPrice.java
@@ -1,24 +1,39 @@
 /**
- * Created with IntelliJ IDEA.
- * User: Berkling
- * Date: 14.05.14
- * Time: 12:48
- * To change this template use File | Settings | File Templates.
+ * Represents the pricing strategy for children's movies.
+ * Applies a specific charge policy based on the number of rental days.
  */
 public class ChildrensPrice implements Price {
 
-
-    public int getPriceCode(){
+    /**
+     * Returns the price code for children's movies.
+     *
+     * @return the price code for children's category
+     */
+    public int getPriceCode() {
         return Movie.CHILDRENS;
     }
 
-    public double getCharge(int daysRented){
-         if (daysRented > 3) return (daysRented - 3 ) * 1.5;
-           else return 1.5;
+    /**
+     * Calculates the charge for renting a children's movie.
+     *
+     * @param daysRented the number of days the movie is rented
+     * @return the charge based on rental duration
+     */
+    public double getCharge(int daysRented) {
+        if (daysRented > 3) {
+            return (daysRented - 3) * 1.5;
+        } else {
+            return 1.5;
+        }
     }
+
+    /**
+     * Returns the frequent renter points for a children's movie.
+     *
+     * @param daysRented the number of days the movie is rented
+     * @return the number of frequent renter points
+     */
     public int getFrequentRenterPoints(int daysRented) {
         return 1;
     }
-
 }
-
